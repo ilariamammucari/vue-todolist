9c79b060b07d445aa1075611d80d4fa19c90710a
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-28 14:53:09 +0100

    senza inserimento da input

--- vue/script.js
+++ vue/script.js
@@ -1,1 +1,30 @@
-alert();
+var app = new Vue({
+    el: '#root',
+    data: {
+        coseDaFare: [
+            'Fare la spesa',
+            'Studiare',
+            'Cena fuori alle 21:00'
+        ],
+        coseCestinate: [],
+        modifica: ''
+    },
+    methods: {
+        cancellaDaFare(index){
+            this.coseCestinate.push(this.coseDaFare[index]);
+            this.coseDaFare.splice(index,1);
+        },
+        modificaDaFare(index){
+            this.modifica = prompt('Modifica cosa da fare');
+            this.modifica = this.modifica.charAt(0).toUpperCase() + this.modifica.substr(1).toLowerCase();
+            this.coseDaFare.splice(index,1,this.modifica);
+        },
+        cancellaDefinitivamente(index){
+            this.coseCestinate.splice(index,1);
+        },
+        ripristina(index){
+            this.coseDaFare.push(this.coseCestinate[index]);
+            this.coseCestinate.splice(index,1);
+        }
+    }
+});
